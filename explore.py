d7b78dd0991316c61107b8d9f020c33230f89a5e
Bryson Rogers <rog14009@byui.edu>
2020-10-15 10:49:59 -0600

    score round and enemies respawn

--- explore.py
+++ explore.py
@@ -5,6 +5,9 @@ import bullet
 import enemy
 import math
 pygame.init()
+pygame.font.init()
+
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
 height = 720
 width = 1080
@@ -17,10 +20,27 @@ FPS = pygame.time.Clock()
 
 player = player.Player(540, 360)
 
-enemyOne = enemy.Enemy(50, 50)
-enemyTwo = enemy.Enemy(1000, 700)
+enemyO = enemy.Enemy(-50, 50)
+enemyT = enemy.Enemy(1000, 740)
 
-    
+enemies = [enemyO, enemyT]
+
+def nextRound(Round, enemies):
+    Round += 1 
+    r = 0               #calls memory error in this function
+    while (r < Round):
+        xO = 1 + r * 2
+        yO = -50
+        enemies.append(enemy.Enemy(xO, yO))
+        xT = width + 50
+        yT = 1 + r
+        enemies.append(enemy.Enemy(xT, yT))
+        xH = -50
+        yH = height - r * 2 
+        enemies.append(enemy.Enemy(xH, yH))
+        xF = width - r * 2
+        yF =  height + 50
+        enemies.append(enemy.Enemy(xF, yF))
 
 
 #game loop
@@ -34,16 +54,25 @@ def gameLoop():
     press_D = False
     press_left = False
     press_right = False
-    press_space = False
+    press_up = False
     delay = False
+    Round = 1
+    score = 0
+
     bullets = []
-    enemies = [enemyOne, enemyTwo]
     
+
+    
+
 
     
     while game:
         x = 0
         y = 0
+        scoreTxt = myfont.render('Score: ', False, grey)
+        scoreNum = myfont.render((str(score)) , False, grey)
+        roundTxt = myfont.render('Round: ', False, grey)
+        roundNum = myfont.render((str(Round)) , False, grey)
         for event in pygame.event.get():
             if (event.type == pygame.QUIT): # Quit is esc or x in the corner
                 game = False 
@@ -72,10 +101,10 @@ def gameLoop():
                 press_right = True
             elif ((event.type == pygame.KEYUP) and (event.key == pygame.K_RIGHT)):
                 press_right = False
-            elif ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_SPACE)):
-                press_space = True
-            elif ((event.type == pygame.KEYUP) and (event.key == pygame.K_SPACE)):
-                press_space = False
+            elif ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_UP)):
+                press_up = True
+            elif ((event.type == pygame.KEYUP) and (event.key == pygame.K_UP)):
+                press_up = False
             
 
 
@@ -92,7 +121,7 @@ def gameLoop():
             player.turnLeft() 
         elif (press_right):  
             player.turnRight() 
-        if (press_space) and player.health != 0:
+        if (press_up) and player.health != 0:
             bullets.append(bullet.Bullet(player.oriX, player.oriY, player.locX, player.locY)) 
 
         if len(bullets) > 0:
@@ -113,15 +142,22 @@ def gameLoop():
             for x in range(len(enemies)):
                 for b in range(len(bullets)):
                     if (math.sqrt((abs(bullets[b].x - enemies[x].x)**2) + abs(bullets[b].y - enemies[x].y)**2) < 10):
+                        score += 1
                         remove = enemies[x]
                         del enemies[x]
                         remove.kill()
                         break
                 break
+        elif (len(enemies) < 1):
+            nextRound(Round, enemies)
 
         
 
         DISPLAYSURF.fill(black)
+        DISPLAYSURF.blit(scoreTxt,(5,5))
+        DISPLAYSURF.blit(scoreNum,(100,5))
+        DISPLAYSURF.blit(roundTxt,(5,55))
+        DISPLAYSURF.blit(roundNum,(100,55))
         if len(bullets) > 0:
             for x in range(len(bullets)):
                 bullets[x].draw(DISPLAYSURF)
